cf87d522806c04170690cf53ef8a462a3593cef4
Alex Iadicicco <alex@ajitek.net>
2014-03-06 12:51:51 -0700

    Add gx-math

---
+++ gx-math/math.h
@@ -0,0 +1,9 @@
+/* gx-math/math.h, Copyright (C) 2014 Alex Iadicicco */
+
+#ifndef __INC_GX_MATH_MATH_H__
+#define __INC_GX_MATH_MATH_H__   1
+
+extern s16 gx_sin(u16 theta);
+extern s16 gx_cos(u16 theta);
+
+#endif
